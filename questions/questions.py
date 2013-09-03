bf400051fd9ec3d75483e1259d90706456bbb450
Ben Lucato <ben.lucato@gmail.com>
2013-09-03 19:27:40 +1000

    removed some commented-out code in questions/questions.py

--- questions/questions.py
+++ questions/questions.py
@@ -1,17 +1,5 @@
 from maths.latex import latex, tex_to_pdf
 
-'''
-def test_question(question_obj):
-    """ Test whether a question's latex is compilable to a PDF.
-        TO BE DEPRECATED SOON in favour of test_question_new
-    """ 
-
-    question_tree = questions.QuestionTree(question_number=1, part=question_obj)
-    with open('test.text', 'w') as f:
-        _write_question(f, question_tree)
-
-    tex_to_pdf.make_pdf('test')'''
-
 
 def test_question(question_tree):
     """ Test whether a question's latex is compilable to a PDF.
