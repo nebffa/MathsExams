1ec3af5049d92b951c873037a2ac0b104bda1d4b
Ben Lucato <ben.lucato@gmail.com>
2013-09-15 12:18:03 +1000

    almost finalised Piecewise and its subproblems

--- plot/plot.py
+++ plot/plot.py
@@ -35,6 +35,7 @@ def numpify(expr):
     numpified = re.sub(r'Abs', r'numpy.abs', numpified)
     return re.sub(r'x', r'x_values', numpified)
 
+
 def asymptote_proof(array):
     ''' 
     Takes a numpy.ndarray and asymptote-proofs the values so we don't plot over asymptotes.
@@ -44,15 +45,18 @@ def asymptote_proof(array):
     array[array > threshold] = numpy.inf
     array[array < -threshold] = numpy.inf
 
+    return array
 
 
-def plot(expr, domain):
-    if len(expr.atoms(sympy.Symbol)) != 1:
-        raise ValueError(r'The supplied expression, {0}, must rely on only one symbol.'.format(expr))
-    else:
-        # ensure we are just plotting y against x for simplicity. We will label axes according to the original symbols though
-        expr = expr.replace(expr.atoms(sympy.Symbol).pop(), x)
+def latex(path):
+    '''
+    Return the latex to include an Encapsulated PostScript (.eps) file as an image.
+    '''
 
+    return r'''\includegraphics{{{0}}}'''.format(path.strip('.eps'))
+
+
+def _blank_plot(domain):
     # make the plot
     fig = plt.figure(1)
     ax = SubplotZero(fig, 111)
@@ -62,21 +66,22 @@ def plot(expr, domain):
     ax.axhline(linewidth=1.7, color="k")
     ax.axvline(linewidth=1.7, color="k")
 
-
     x_lower, x_upper = int(domain.left), int(domain.right)  # needs to be changed, is just a temporary type changer
     y_lower, y_upper = -5, 5
 
+    # remove tick lines on the axes
     plt.xticks([])
     plt.yticks([])
     plt.ylim(y_lower, y_upper)
     plt.xlim(x_lower, x_upper)
-    x_width = (abs(plt.xlim()[0]) + abs(plt.xlim()[1]))
-    y_width = (abs(plt.ylim()[0]) + abs(plt.ylim()[1]))
 
+    # add axes labels
     ax.text(1.05, 0, r'$x$', transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
     ax.text(0, 1.05, r'$y$', transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
 
     # end-of-axis arrows
+    x_width = (abs(plt.xlim()[0]) + abs(plt.xlim()[1]))
+    y_width = (abs(plt.ylim()[0]) + abs(plt.ylim()[1]))
     plt.arrow(plt.xlim()[1], -0.003, 0.00000000001, 0,
               width=x_width*0.0015*0.5, color="k", clip_on=False,
               head_width=y_width*0.12/7, head_length=x_width*0.024*0.5)
@@ -84,18 +89,56 @@ def plot(expr, domain):
               width=y_width*0.0015*0.5, color="k", clip_on=False,
               head_width=x_width*0.12/7, head_length=y_width*0.024*0.5)
 
+    # only show cartesian axes
     for direction in ["xzero", "yzero"]:
         ax.axis[direction].set_visible(True)
-
     for direction in ["left", "right", "bottom", "top"]:
         ax.axis[direction].set_visible(False)
+
+
+def _save_plot():
+    # save the image and return the path name so the caller can include it in the latex
+    uid = uuid.uuid1()
+    path = os.path.join(r'C:\Users\Ben\Desktop\Dropbox\maths\questions\figures', str(uid) + '.eps')
+    plt.savefig(path)
+
+    return path
+
+
+def blank_plot(domain):
+    _blank_plot(domain)
+    path = _save_plot()
+
+    return path
+
+
+
+def plot(expr, plot_domain, expr_domain=None):
+    if len(expr.atoms(sympy.Symbol)) != 1:
+        raise ValueError(r'The supplied expression, {0}, must rely on only one symbol.'.format(expr))
+    else:
+        # ensure we are just plotting y against x for simplicity. We will label axes according to the original symbols though
+        expr = expr.replace(expr.atoms(sympy.Symbol).pop(), x)
+
+    if plot_domain.left == -sympy.oo or plot_domain.right == sympy.oo:
+        raise ValueError('The supplied plot_domain goes to infinity: {0}'.format(plot_domain))
+
+    if expr_domain is None:
+        expr_domain = plot_domain
+    elif expr_domain.left == -sympy.oo or expr_domain.right == sympy.oo:
+        raise ValueError('The supplied expr_domain goes to infinity: {0}'.format(plot_domain))
+
+
+    _blank_plot(plot_domain)    
+
 
     # plot each part of a piecewise individually    
     if isinstance(expr, sympy.Piecewise):
 
         # get a list of the ends of domains for the piecewise parts
-        domains = [numpy.float64( i[1].rhs ) for i in expr.args] + [plt.xlim()[1]]
-        domains.insert(0, plt.xlim()[0])
+        domains = [numpy.float64( i[1].rhs ) for i in expr.args]
+        domains.insert(0, expr_domain.left)
+        domains.append(expr_domain.right)
         domains = f7(domains)
 
         for i in range(len(expr.args)):
@@ -107,29 +150,19 @@ def plot(expr, domain):
             #x_values = numpy.delete(undefined_indices)
             
             y_values = eval(r'{0}'.format(numpified))
-            
-            threshold = 1000
-            y_values[y_values > threshold] = numpy.inf
-            y_values[y_values < -threshold] = numpy.inf
-
-            
+            y_values = asymptote_proof(y_values)
+    
             plt.plot(x_values, y_values, color="k")
 
     else:
         numpified = numpify(expr)
 
-        x_values = numpy.linspace(plt.xlim()[0], plt.xlim()[1], num=10000)
+        x_values = numpy.linspace(expr_domain.left, expr_domain.right, num=10000)
+
         y_values = eval(r'{0}'.format(numpified))
-        
-        threshold = 1000
-        y_values[y_values > threshold] = numpy.inf
-        y_values[y_values < -threshold] = numpy.inf
+        y_values = asymptote_proof(y_values)
 
         plt.plot(x_values, y_values, color="k")
 
-    # save the image and return the path name so the caller can include it in the latex
-    uid = uuid.uuid1()
-    path = os.path.join(r'C:\Users\Ben\Desktop\Dropbox\maths\questions\figures', str(uid) + '.eps')
-    plt.savefig(path)
-
+    path = _save_plot()
     return path
