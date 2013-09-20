abc60109514a323422f1d149ae6cf09d99108c0c
Ben Lucato <ben.lucato@gmail.com>
2013-09-20 16:11:40 +1000

    added questtions/question.py, a place to test individual questions

---
+++ questions/question.py
@@ -0,0 +1,18 @@
+from maths.latex import latex, questions
+from maths.parts import (
+                            definite_integral_equality,
+                            worded_definite_integral
+                        )
+
+
+with open('exam.tex', 'w') as f:
+    latex.begin_tex_document(f)
+
+    q = worded_definite_integral.WordedDefiniteIntegral()
+    question = questions.QuestionTree(q)
+    question.write_question(f)
+    question.write_solution(f)
+    latex.new_page(f)
+
+
+    latex.end_tex_document(f)
