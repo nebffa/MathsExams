dc51010ce1859759659b35ac674acf695496e7b7
Ben Lucato <ben.lucato@gmail.com>
2013-10-07 19:42:32 +1100

    moved /names to /questionmisc

--- questionmisc/first_names.py
+++ questionmisc/first_names.py
@@ -7,13 +7,23 @@ def full_path():
     return os.getcwd()
 
 
-def read_names():
+def read_names(gender=False):
+    ''' Return a list of first names, with their corresponding genders if gender is True
+    '''
     dirname = os.path.split(__file__)[0]
     path = os.path.join(dirname, 'first_names.pickle')
 
     with open(path, 'rb') as f:
-        return pickle.load(f)
+        if gender:
+            return pickle.load(f)
+        else:
+            return [i[0] for i in pickle.load(f)]
 
 
-def random_name():
-    return random.choice(read_names())
+def random_first_name(gender=False):
+    ''' Return a random first name, including the gender if gender is True.
+    '''
+    if gender:
+        return random.choice(read_names(gender))
+    else:
+        return random.choice(read_names(gender))[0]
