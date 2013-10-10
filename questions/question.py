605b9ad547fdd7f37eacdedadf3645c0d7247756
Ben Lucato <ben.lucato@gmail.com>
2013-10-10 15:54:25 +1100

    introduced sublime-text projects, modified the gitignore

--- questions/question.py
+++ questions/question.py
@@ -1,20 +1,16 @@
 from maths.latex import latex, questions
 from maths.parts import (
-                            simple_sketch
+                            markov_chain
                         )
 
 
 with open('exam.tex', 'w') as f:
     latex.begin_tex_document(f)
 
-    q = simple_sketch.SimpleSketch()
+    q = markov_chain.MarkovChainBinomial()
     question = questions.QuestionTree()
     question.add_part(q)
-    
-    question.add_part()
-    
-    question.add_part(simple_sketch.PointTransformation(q), tree_location=1)
-    question.add_part(simple_sketch.EquationTransformation(q), tree_location=1)
+
     question.write_question(f)
     latex.new_page(f)
     question.write_solution(f)
