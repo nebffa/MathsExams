9291551fe4c624c000cf753728102396abbf7389
Ben Lucato <ben.lucato@gmail.com>
2013-10-10 17:10:26 +1100

    new individual_question folder

---
+++ questions/individual_questions/piecewise_prob_density_function.py
@@ -0,0 +1,18 @@
+from maths.latex import latex, questions
+from maths.parts import (
+                            piecewise_prob_density_function
+                        )
+
+
+with open('exam.tex', 'w') as f:
+    latex.begin_tex_document(f)
+
+    q = piecewise_prob_density_function.PiecewiseProbDensityFunction()
+    question = questions.QuestionTree(q)
+
+
+    question.write_question(f)
+    latex.new_page(f)
+    question.write_solution(f)
+
+    latex.end_tex_document(f)
