466a6f7ff30ce55332b470c473d7c83d292db571
Ben Lucato <ben.lucato@gmail.com>
2013-10-12 09:54:51 +1100

    added an individual_question for markov_chain

---
+++ questions/individual_questions/markov_chain.py
@@ -0,0 +1,18 @@
+from maths.latex import latex, questions
+from maths.parts import (
+                            markov_chain
+                        )
+
+
+with open('exam.tex', 'w') as f:
+    latex.begin_tex_document(f)
+
+    q = markov_chain.MarkovChainBinomial()
+    question = questions.QuestionTree(q)
+
+
+    question.write_question(f)
+    latex.new_page(f)
+    question.write_solution(f)
+
+    latex.end_tex_document(f)
