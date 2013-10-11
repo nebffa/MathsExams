f1c23b91dc0571ec40e4750ded8ef0202fb2c6d6
Ben Lucato <ben.lucato@gmail.com>
2013-10-12 09:52:52 +1100

    adjusted the question template

--- templates/question_template.py
+++ templates/question_template.py
@@ -1,5 +1,21 @@
 import sympy
 import random
 from sympy.abc import *
-from maths import all_functions, not_named_yet, simplify
+from maths import all_functions, not_named_yet
 
+
+class name:
+    def __init__(self):
+        self.num_lines, self.num_marks = -1, -1
+
+
+
+
+    def question_statement(self):
+        pass
+
+
+
+
+    def solution_statement(self):
+        pass
