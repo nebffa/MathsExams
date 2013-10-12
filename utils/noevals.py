e4687b63464e5de5a6b8f82cf15f567f56d37d9a
Ben Lucato <ben.lucato@gmail.com>
2013-10-13 10:34:52 +1100

    added comments about what is wrong with noevals.py'

--- utils/noevals.py
+++ utils/noevals.py
@@ -1,41 +1,6 @@
 import sympy
 
-'''
-I know this is looking back a month or two, but I have just started to use your idea about subclassing.
-
-However, it is having unintended side effects.
-
-Including the code:
-
-class exp((sympy.exp):
-    @classmethod
-    def eval(cls, arg):
-        return
-
-in a module like noevals.py, and then subsequently importing noevals has disabled eval in the regular (sympy.exp. 
-I am super new to subclassing in general, but I know it's this code because when I delete these 4 lines the problem goes away. 
-How do I retain this subclass without altering the parent's behaviour?
-
-
----------------------------------------------------------------------------------------------------------------------
-
-
-I can only guess at what's happening, but does it work if you don't 
-name the class "exp"? There is (unfortunately) a global class registry 
-in SymPy that uses the name of the class, so a subclass called "exp" 
-might inadvertently override exp for other parts of SymPy. 
-
-If this is the issue, you can always call the class something like 
-UnevaluatedExp, and then put "exp = UnevaluatedExp" at the end of the 
-definition for convenience (or always import it as "from module import 
-UnevaluatedExp as exp"). The important thing is the __name__ of the 
-class. 
-
-Of course, we really should get rid of the class registry. But it 
-hasn't been done yet. 
-
-@ https://groups.google.com/forum/#!topic/sympy/zilEXwN26so
-'''
+# calls to the printers aren't working
 
 
 def noevalsub(expr, mapping):
@@ -102,6 +67,7 @@ class noevalAdd(sympy.Add):
         return seq, [], None
 
 
+# doesn't work with negative arguments
 class noevalMul(sympy.Mul):
     @classmethod
     def flatten(cls, seq):
@@ -121,5 +87,6 @@ print(dir(y))
 print(y)'''
 
 
+# doesn't work at all
 class noevalPow(sympy.Pow):
     pass
