39afa9b0ae4b6fae864145b9da04f9bc0c67ba38
Ben Lucato <ben.lucato@gmail.com>
2013-10-14 17:26:50 +1100

    some new question tests as well and the test for simple sketch is now good to go

--- parts/tests/test_piecewise.py
+++ parts/tests/test_piecewise.py
@@ -1,9 +1,8 @@
-from maths.parts import simple_sketch
+from maths.parts import piecewise
 from maths.latex.questions import QuestionTree
 from .question_tester import question_tester
 
 
-
-def test_SimpleSketch():
-    q1 = simple_sketch.SimpleSketch()
+def test_Piecewise():
+    q1 = piecewise.Piecewise()
     question_tester(QuestionTree(q1))
