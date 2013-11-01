cb3bb87d780593dc314c6cc761e0a902c5818ca9
Ben Lucato <ben.lucato@gmail.com>
2013-11-01 13:58:39 +1100

    the folder for viewing latex output of individual questions is now done in just one folder

--- questions/individual_questions/piecewise_prob_density_function.py
+++ questions/individual_questions/piecewise_prob_density_function.py
@@ -7,10 +7,21 @@ from maths.parts import (
 with open('exam.tex', 'w') as f:
     latex.begin_tex_document(f)
 
-    q = piecewise_prob_density_function.PiecewiseProbDensityFunction()
+    q = piecewise_prob_density_function.PiecewiseProbDensityFunctionKnown()
     question = questions.QuestionTree(q)
 
     question.add_part(piecewise_prob_density_function.Conditional(q))
+
+    question.add_part(piecewise_prob_density_function.Cumulative(q))
+
+
+    question.write_question(f)
+    latex.new_page(f)
+    question.write_solution(f)
+
+
+    q = piecewise_prob_density_function.PiecewiseProbDensityFunctionUnknown()
+    question = questions.QuestionTree(q)
 
 
     question.write_question(f)
