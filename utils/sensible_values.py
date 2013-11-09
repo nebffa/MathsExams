d2747fd01ed80cf6c566c03dc9203b2f08b9bd9a
Ben Lucato <ben.lucato@gmail.com>
2013-11-09 15:03:11 +1100

    completed log_misc

--- utils/sensible_values.py
+++ utils/sensible_values.py
@@ -9,97 +9,102 @@ MAX_OUTPUT = 30
 
 
 def derivative(expr, domain):
-	deriv = expr.diff()
+    deriv = expr.diff()
 
-	return _delegate(deriv, domain)
+    return _delegate(deriv, domain)
 
 
 def antiderivative(expr, domain):
-	antideriv = expr.integrate()
+    antideriv = expr.integrate()
 
-	return _delegate(antideriv, domain)
+    return _delegate(antideriv, domain)
 
 
 def standard(expr, domain):
-	return _delegate(expr, domain)
+    return _delegate(expr, domain)
 
 
 def conditional_integral(expr, domain):
-	'''	Return two points within the domain (excluding the end-points) on which to perform a conditional integral from one side of the domain.
-	'''
+    '''    Return two points within the domain (excluding the end-points) on which to perform a conditional integral from one side of the domain.
+    '''
 
-	modified_domain = sympy.Interval(domain.left, domain.right, True, True)
+    modified_domain = sympy.Interval(domain.left, domain.right, True, True)
 
-	return _delegate(expr, modified_domain, num=2)
+    return _delegate(expr, modified_domain, num=2)
 
 
 
 
 
 def _delegate(expr, domain, num=1):
-	trigs = (sympy.sin, sympy.cos, sympy.tan, sympy.cot, sympy.sec, sympy.csc)
+    trigs = (sympy.sin, sympy.cos, sympy.tan, sympy.cot, sympy.sec, sympy.csc)
 
-	for trig in trigs:
-		if expr.find(trig):
-			return _trig(expr, domain, num)
+    for trig in trigs:
+        if expr.find(trig):
+            return _trig(expr, domain, num)
 
-	if expr.find(sympy.log):
-		pass
+    if expr.find(sympy.log):
+        pass
 
-	if expr.find(sympy.exp):
-		pass
+    if expr.find(sympy.exp):
+        pass
 
-	return _polynomial(expr, domain, num)		
+    return _polynomial(expr, domain, num)        
 
 
 def looks_good(value):
-	if isinstance(value, sympy.Rational):
-		if not (-MAX_OUTPUT < value.p < MAX_OUTPUT):
-			return False
+    if not sympy.ask(sympy.Q.real(value)):
+        return False
 
-		elif not (-MAX_OUTPUT < value.q < MAX_OUTPUT):
-			return False
+    if isinstance(value, sympy.Rational):
+        if not (-MAX_OUTPUT < value.p < MAX_OUTPUT):
+            return False
 
-		return True
+        elif not (-MAX_OUTPUT < value.q < MAX_OUTPUT):
+            return False
 
-	elif isinstance(value, (sympy.Integer, int)):
-		return True if -MAX_OUTPUT < value < MAX_OUTPUT else False
+        return True
+
+    elif isinstance(value, (sympy.Integer, int)):
+        return True if -MAX_OUTPUT < value < MAX_OUTPUT else False
+
+
 
 
 
 
 def _polynomial(expr, domain, num=1):
 
-	points = set()
+    points = set()
 
 
-	for denom in range(1, MAX_INPUT + 1):
-		for numerator in range(-MAX_INPUT, MAX_INPUT + 1):
-			frac = sympy.Rational(numerator, denom)
+    for denom in range(1, MAX_INPUT + 1):
+        for numerator in range(-MAX_INPUT, MAX_INPUT + 1):
+            frac = sympy.Rational(numerator, denom)
 
-			if frac not in points:
-				points.add(frac)
-
-
-	good_choices = []
-	for point in points:
-
-		if point not in domain:
-			continue
-
-		evaluated_expr = expr.subs({x: point})
-
-		if looks_good(evaluated_expr):
-			good_choices.append(point)
+            if frac not in points:
+                points.add(frac)
 
 
-	return random.sample(good_choices, num)
+    good_choices = []
+    for point in points:
+
+        if point not in domain:
+            continue
+
+        evaluated_expr = expr.subs({x: point})
+
+        if looks_good(evaluated_expr):
+            good_choices.append(point)
 
 
+    return random.sample(good_choices, num)
 
 
 
-	
+
+
+    
 def _trig(expr, domain, num=1):
 
     inner_function = [particle.func for particle in expr.atoms(sympy.Function)][0]
@@ -127,8 +132,8 @@ def _trig(expr, domain, num=1):
 
 
 def _exp(expr, domain, num=1):
-	pass
+    pass
 
 
 def _log(expr, domain, num=1):
-	pass
+    pass
