b6cced184b95e6f00e260b2be4028050a5a52d94
Ben Lucato <ben.lucato@gmail.com>
2013-11-20 13:18:33 +1100

    added some more tests

--- parts/tests/test_sketch_misc.py
+++ parts/tests/test_sketch_misc.py
@@ -1,8 +1,12 @@
-from maths.parts import sketch_misc
+from maths.parts import sketch_misc, simple_sketch
 from maths.latex.questions import QuestionTree
 from .question_tester import question_tester
+import sympy
 
 
 def test_SketchMisc():
-    q1 = sketch_misc.SketchMisc()
+    base_q = simple_sketch.SimpleSketch()
+    base_q._qp['domain'] = sympy.Interval(-sympy.oo, sympy.oo)
+
+    q1 = sketch_misc.SketchDoubleInverse(base_q)
     question_tester(QuestionTree(part=q1))
--- parts/tests/test_trig_properties.py
+++ parts/tests/test_trig_properties.py
@@ -1,8 +1,8 @@
-from maths.parts import log_misc
+from maths.parts import trig_properties
 from maths.latex.questions import QuestionTree
 from .question_tester import question_tester
 
 
-def test_LogMisc():
-    q1 = log_misc.SolveLogEquation()
+def test_TrigProperties():
+    q1 = trig_properties.TrigProperties()
     question_tester(QuestionTree(part=q1))
