7d28f0afda603d7635e22ff976beee0d73689536
Ben Lucato <ben.lucato@gmail.com>
2013-11-27 19:20:43 +1100

    changed path of question tests

--- parts/tests/question_tester.py
+++ parts/tests/question_tester.py
@@ -3,14 +3,17 @@ import uuid
 import os
 import subprocess
 import errno
+from maths import maths_path
+import glob
 
 
-def question_tester(question_tree):
+def question_tester(question_tree, view_output=False):
     """ Test whether a question's latex is compilable to a PDF.
 
     """ 
 
-    full_path = os.path.split(os.path.abspath(__file__))[0]
+    #full_path = os.path.split(os.path.abspath(__file__))[0]
+    full_path = os.path.join(maths_path.maths_path(), 'debug')
     uid = str(uuid.uuid1())
 
     file_name = os.path.join(full_path, 'test' + uid + '.tex')
@@ -19,19 +22,38 @@ def question_tester(question_tree):
         _write_question(f, question_tree)
 
 
-    # change dir to C:\Users\Ben\Desktop\Dropbox\maths\parts\tests so that the byproduct tex compilation files are created there
+    # change dir to ../maths/debug so that the byproduct tex compilation files are created there
     # in case something goes wrong in compilation and we want to view them
     prev_path = os.getcwd()
     os.chdir(full_path)
-    retcode = subprocess.call(['xelatex', '-halt-on-error', file_name])
+    
+    with open(os.devnull, 'w') as f:
+        retcode = subprocess.call(['xelatex', '-halt-on-error', file_name], stdout=f)
+
     if retcode != 0:
+        # wipe all other files in ../maths/debug so we can easily find the files we want
+
+        keep_files = [os.path.join(full_path, 'test' + uid + i) for i in ['.log', '.aux', '.tex']]
+
+        for filename in glob.glob(os.path.join(full_path, '*')):
+            if filename not in keep_files:
+                os.remove(filename)
+
+        subprocess.Popen(['texmaker', os.path.join(full_path, 'test' + uid + '.tex')])
+
         raise RuntimeError("This questions' latex could not be compiled.")
 
+
+
+    if view_output:
+        subprocess.call(['evince', os.path.join(full_path, 'test' + uid + '.pdf')])
+        # if retcode != 0:
+        #     pass
+            #subprocess.call([''])
 
     # cleanup after .tex compilation
     silentremove(os.path.join(full_path, 'test' + uid + '.log'))
     silentremove(os.path.join(full_path, 'test' + uid + '.aux'))
-    #silentremove(os.path.join(full_path, 'test' + uid + '.pdf'))
     silentremove(os.path.join(full_path, 'test' + uid + '.tex'))
 
     os.chdir(prev_path)
@@ -40,26 +62,16 @@ def question_tester(question_tree):
 def silentremove(filename):
     try:
         os.remove(filename)
-    except OSError as e: # this would be "except OSError as e:" in python 3.x
-        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-            raise # re-raise exception if a different error occured
+    except OSError as e:
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # re-raise exception if a different error occured
 
 
 def _write_question(f, question_tree):
     """ A helper function to test_question - writes a single question as a standalone .tex file.
-
     """
 
-    #f.write('asdasfasfuhsafjo12313<1k23124$$$$$$')
     latex.begin_tex_document(f)
     question_tree.write_question(f)
     question_tree.write_solution(f)
     latex.end_tex_document(f)
-
-
-def to_string(lines):
-    for i in range(len(lines)):
-        if i != len(lines) - 1:
-            lines[i] += latex.latex_newline()
-
-    return ''.join(lines)
