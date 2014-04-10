38097c117cd9157676290dd1295718c81e2163d9
Ben Lucato <ben.lucato@gmail.com>
2014-04-11 00:34:35 +1000

    Cleanup the code for the antiderivative question module.

--- maths/questions/antiderivative.py
+++ maths/questions/antiderivative.py
@@ -1,70 +1,74 @@
 import sympy
 import random
 from .. import all_functions, not_named_yet
-from ..latex import latex
-from sympy.abc import *
+from ..latex import latex, solution_lines
+from ..symbols import x0
 from . import relationships
 
 
 @relationships.root
 class Antiderivative:
+    """
+    Question description
+    ====================
+
+    Take a simple expression and find its antiderivative.
+
+
+    Real-life instances
+    ===================
+
+    2009 2a: y = 1 / (1 - 2x) [4 lines] [2 marks]
+    2010 2a: y = cos(2x + 1) [2 lines] [1 mark]
+    2011 2a: y = 1 / (3x - 4) [5 lines] [1 mark]
+    2012 2: y = 1 / (2x - 1)^3 [4 lines] [2 marks]
+
+    there is no correlation between function type and marks assigned, so we have to choose between
+    the historic 1 or 2 marks this question has been assigned
+    """
+
     def __init__(self):
-
-        # there is no correlation between function type and marks assigned, so we have to choose between
-        # the historic 1 or 2 marks this question has been assigned
         self.num_marks = random.randint(1, 2)
-        self.num_lines = 4
+        self._qp = {}
 
-        self._function_type = random.choice(['linear', 'trig'])
+        self._qp['function_type'] = random.choice(['linear', 'trig'])
 
         inner_function = all_functions.request_linear(difficulty=3).equation
-        if self._function_type == 'linear':
-            # 2009 2a: y = 1 / (1 - 2x) [4 lines] [2 marks]
-            # 2011 2a: y = 1 / (3x - 4) [5 lines] [1 mark]
-            # 2012 2: y = 1 / (2x - 1)^3 [4 lines] [2 marks]
+        if self._qp['function_type'] == 'linear':
+
             self.num_lines = 4
 
             index = random.randint(1, 3)
 
-            self.equation = 1 / inner_function ** index
+            self._qp['equation'] = 1 / inner_function ** index
 
-        elif self._function_type == 'trig':
-            # 2010 2a: y = cos(2x + 1) [2 lines] [1 mark]
+        elif self._qp['function_type'] == 'trig':
+
             self.num_lines = 2
 
             outer_function = random.choice([sympy.cos, sympy.sin])
-            self.equation = outer_function(inner_function)
+            self._qp['equation'] = outer_function(inner_function)
 
-        self.antiderivative = self.equation.integrate()
+        self._qp['antiderivative'] = self._qp['equation'].integrate()
 
-    # in all years but 2012, question 2 has been composed of two unrelated parts - we need to signal to write_question
-    # whether is a subpart or whether it is an entire question
     def question_statement(self):
-
-        # sympy prints things like (1 + 2x)^-2 as exactly that, rather than in fraction form, so we have to manually
-        # print it as a fraction
-        if self._function_type == 'linear':
-            inner_function, index = self.equation.as_base_exp()
-            # don't print the index if it's one!!!
-            if abs(index) == 1:
-                equation = r"\frac{1}{%s}" % sympy.latex(inner_function)
-            else:
-                equation = r"\frac{1}{\left(%s\right)^{%d}}" % (sympy.latex(inner_function), abs(index))
-        else:
-            equation = sympy.latex(self.equation)
-
-        return 'Find an antiderivative of $%s$ with respect to $x$.' % equation
+        return 'Find an antiderivative of ${equation}$ with respect to $x$.'.format(
+            equation=sympy.latex(self._qp['equation'])
+        )
 
     def solution_statement(self):
-        # sympy integrates things like 1/x as log(x), not log(|x|) (the explanation given to me was that it was due to complex numbers)
+        # sympy integrates things like 1/x as log(x), not log(|x|) (since the symbol x is treated as a complex number, not a real number)
+        proper_antiderivative = self._qp['antiderivative'].replace(sympy.log(x0), sympy.log(sympy.Abs(x0)))
 
-        proper_antiderivative = self.antiderivative.replace(sympy.log(a), sympy.log(sympy.Abs(a)))
-        constant_of_integration = not_named_yet.randint(-3, 3, exclude=[0])
+        constant_of_integration = not_named_yet.randint_no_zero(-3, 3)
 
-        line_1 = r'${0}$'.format(sympy.latex(proper_antiderivative.factor() + constant_of_integration))
-        line_2 = r'We arbitrarily choose our constant of integration to be ${0}$. It can be any real number, including zero.'.format(constant_of_integration)
+        lines = solution_lines.Lines()
 
-        return latex.latex_newline().join([line_1, line_2])
+        # without using .factor() here, we could have (x + 1)**(-3) integrate to -1/(2*x**2 + 4*x + 2) which is expanded
+        antiderivative = proper_antiderivative.factor() + constant_of_integration
+        lines += r'${antiderivative}$'.format(antiderivative=sympy.latex(antiderivative))
+        lines += r'We arbitrarily choose our constant of integration to be ${constant_of_integration}$. It can be any real number, including zero.'.format(
+            constant_of_integration=constant_of_integration
+        )
 
-    def sanity_check(self):
-        pass
+        return lines.write()
