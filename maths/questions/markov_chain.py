d8db47337f1f4d71007ebd1482742b36a7d997f0
Ben Lucato <ben.lucato@gmail.com>
2014-04-17 15:46:53 +1000

    Cleanup code in markov_chain and add some tests

--- maths/questions/markov_chain.py
+++ maths/questions/markov_chain.py
@@ -4,138 +4,184 @@ import decimal
 import itertools
 import functools
 import operator
-from ..symbols import *
 from ..phrasing import days, first_names
-from ..latex import latex
+from ..latex import latex, solution_lines
 from . import relationships
 
 
 @relationships.root
-class MarkovChainBinomial:
+class MarkovChainBinomial(relationships.QuestionPart):
+    """
+    Question description
+    ====================
+
+    Calculate the probability of getting "x" successes out of "n" trials using Markov chains.
+
+
+    Real-life instances
+    ===================
+
+    2008 8: [10 lines] [3 marks]
+    """
+
+    @staticmethod
+    def all_paths(start_state, n_trials):
+        """Return all paths the Markov chain can take in a number of trials.
+        """
+        paths = [[start_state]]
+        length = 1
+
+        while length < n_trials:
+            unflattened_paths = ([state + [0], state + [1]] for state in paths)
+            paths = itertools.chain.from_iterable(unflattened_paths)
+            length += 1
+
+        return list(paths)
+
+    @staticmethod
+    def cinema_path(path, location_names):
+        """Return LaTeX that shows the set of cinemas a path visits.
+        """
+
+        individual_trials = [r'Pr(X_{{{week_number}}} = \text{{{cinema_name}}})'.format(
+            week_number=key,
+            cinema_name=location_names[value])
+            for key, value in enumerate(path)
+        ]
+
+        return r' \times '.join(individual_trials)
+
+    @staticmethod
+    def numeric_path(path, transition_matrix):
+        """Return LaTeX that shows the probabilities of taking a particular path.
+        """
+
+        numerics = []
+        for i in range(1, len(path)):
+            transition = transition_matrix[(path[i - 1], path[i])]
+            numerics.append(transition)
+
+        return r' \times '.join([str(i) for i in numerics])
+
+    @staticmethod
+    def path_value(path, transition_matrix):
+        """Return the probability of a path happening.
+        """
+
+        transitions = []
+        for i in range(1, len(path)):
+            transitions.append(transition_matrix[(path[i - 1], path[i])])
+
+        return functools.reduce(operator.mul, transitions)
 
     def __init__(self):
         self.num_lines, self.num_marks = 10, 3
-
         self._qp = {}
 
         # in range 0.10, 0.15, ..., 0.90
-        amounts = random.sample([decimal.Decimal('0.05')*i for i in range(2, 19)], 2)
+        amounts = random.sample([decimal.Decimal('0.05') * i for i in range(2, 19)], 2)
         prob_place1_becomes_place1 = amounts[0]
         prob_place1_becomes_place2 = 1 - prob_place1_becomes_place1
-
 
         prob_place2_becomes_place1 = amounts[1]
         prob_place2_becomes_place2 = 1 - prob_place2_becomes_place1
 
-
-        self._qp['probabilities'] = {
+        self._qp['transition_matrix'] = {
             (0, 0): prob_place1_becomes_place1,
             (0, 1): prob_place1_becomes_place2,
             (1, 0): prob_place2_becomes_place1,
             (1, 1): prob_place2_becomes_place2
         }
 
-
-
         self._qp['start_state'] = random.randint(0, 1)
 
-        # just leaving the total at 3 for now
-        total = random.randint(3, 3)
+        # just leaving the n_trials at 4 for now
+        n_trials = 3
         self._qp['binomial'] = {
-            'cinema': random.randint(0, 1),
-            'num_successes': random.choice([1, total - 1]),
-            'num_trials': total
+            'cinema': self._qp['start_state'],
+            'n_successes': 2,
+            'n_trials': n_trials
         }
-
 
     def question_statement(self):
         self._qp['cinemas'] = ['Pollos', 'Altiplano']
-        
-        day = days.random_day()
-        first_name, gender = first_names.random_first_name(gender=True)
-        start_cinema = self._qp['cinemas'][self._qp['start_state']]
 
+        day_of_the_week = days.random_day()
+        first_name, gender = first_names.random_first_name(gender=True)
+
+        start_cinema = self._qp['cinemas'][self._qp['start_state']]
         target_cinema = self._qp['cinemas'][self._qp['binomial']['cinema']]
 
-        pronoun = 'he' if gender == 'male' else 'she'
+        pronoun_for_person = 'he' if gender == 'male' else 'she'
 
         self._qi = {
-            'day': day,
+            'day_of_the_week': day_of_the_week,
             'first_name': first_name,
-            'gender': gender,
         }
 
-        
+        setup = r'''Every {day_of_the_week} {first_name} goes to see a movie. {pronoun_for_person} always goes to one of two nearby cinemas - the
+        {first_cinema_name} or the {second_cinema_name}.
 
-        setup = r'''Every {0} {1} goes to see a movie. {2} always goes to one of two nearby cinemas - the {3} or the {4}.
+        If {pronoun_for_person} goes to the {first_cinema_name} one {day_of_the_week}, the probability that {pronoun_for_person} goes to the
+        {second_cinema_name} the next week is {first_to_second_transition_probability}. If {pronoun_for_person} goes to the {second_cinema_name}
+        one {day_of_the_week}, then the probability that {pronoun_for_person} goes to the {first_cinema_name} the next {day_of_the_week} is
+        {second_to_first_transition_probability}.
 
-        If {2} goes to the {3} one {0}, the probability that {2} goes to the {4} is {5}. If {2} goes to the {4} one {0}, 
-        then the probability that {2} goes to the {3} the next {0} is {6}.
+        On any given {day_of_the_week} the cinema {pronoun_for_person} goes to depends only on the cinema {pronoun_for_person} went to on the
+        previous {day_of_the_week}.'''.format(
+            day_of_the_week=day_of_the_week,
+            first_name=first_name,
+            pronoun_for_person=pronoun_for_person,
+            first_cinema_name=self._qp['cinemas'][0],
+            second_cinema_name=self._qp['cinemas'][1],
+            first_to_second_transition_probability=self._qp['transition_matrix'][(0, 1)],
+            second_to_first_transition_probability=self._qp['transition_matrix'][(1, 0)]
+        )
 
-        On any given {0} the cinema {2} goes to depends only on the cinema {2} went to on the previous {0}.
-        '''.format(day, first_name, pronoun, self._qp['cinemas'][0], self._qp['cinemas'][1], 
-                    self._qp['probabilities'][(0, 1)], self._qp['probabilities'][(1, 0)])
-
-
-        question = r'''If {0} goes to the {1} one {2}, what is the probability that {0} goes to the {3} on 
-        exactly {4} of the next {5} {2}s?'''.format(pronoun, start_cinema, day, target_cinema, 
-                self._qp['binomial']['num_successes'], self._qp['binomial']['num_trials'])
-
+        question = r'''If {pronoun_for_person} goes to the {start_cinema_name} one {day_of_the_week},
+        what is the probability that {pronoun_for_person} goes to the {target_cinema_name} on
+        exactly {number_of_cinema_trips} of the next {number_of_weeks} {day_of_the_week}s?'''.format(
+            pronoun_for_person=pronoun_for_person,
+            start_cinema_name=start_cinema,
+            day_of_the_week=day_of_the_week,
+            target_cinema_name=target_cinema,
+            number_of_cinema_trips=self._qp['binomial']['n_successes'],
+            number_of_weeks=self._qp['binomial']['n_trials']
+        )
 
         return setup + latex.latex_newline() + question
 
-
     def solution_statement(self):
+        lines = solution_lines.Lines()
 
-        states = [ [0], [1] ]
-
-
-        
-
-        while len(states[0]) < self._qp['binomial']['num_trials']:
-            unflattened_states = [[state + [0], state + [1]] for state in states]
-            states = list( itertools.chain.from_iterable(unflattened_states) )
-
+        paths = MarkovChainBinomial.all_paths(self._qp['start_state'], self._qp['binomial']['n_trials'])
         if self._qp['binomial']['cinema'] == 0:
-            solution_paths = list(filter(lambda x: len(x) - sum(x) == self._qp['binomial']['num_successes'], states))
+            solution_paths = [i for i in paths if len(i) - sum(i) == self._qp['binomial']['n_successes']]
         else:
-            solution_paths = list(filter(lambda x: sum(x) == self._qp['binomial']['num_successes'], states))
+            solution_paths = [i for i in paths if sum(i) == self._qp['binomial']['n_successes']]
 
+        lines += r'''Let $X$ represent the number of times {first_name} goes to {target_cinema_name} over the ${number_of_weeks}$ {day_of_the_week}s'''.format(
+            first_name=self._qi['first_name'],
+            target_cinema_name=self._qp['cinemas'][self._qp['binomial']['cinema']],
+            number_of_weeks=self._qp['binomial']['n_trials'],
+            day_of_the_week=self._qi['day_of_the_week']
+        )
 
-        path_strings = []
-        for path in solution_paths:
-            product_parts = [r'Pr(X_{{{0}}} = \text{{{1}}})'.format(k, self._qp['cinemas'][v]) for k, v in enumerate(path)]
-            product = r' \times '.join(product_parts)
+        path_strings = [MarkovChainBinomial.cinema_path(path, self._qp['cinemas']) for path in solution_paths]
+        lines += r'''$Pr(X = {number_of_cinema_trips}) = {possible_paths}$'''.format(
+            number_of_cinema_trips=self._qp['binomial']['n_successes'],
+            possible_paths=' + '.join(path_strings)
+        )
 
-            path_strings.append(product)
+        numeric_strings = [MarkovChainBinomial.numeric_path(path, self._qp['transition_matrix']) for path in solution_paths]
+        lines += r'''$= {intermediate_paths}$'''.format(
+            intermediate_paths=' + '.join(numeric_strings)
+        )
 
-
-        numeric_strings = []
-        answer = 0
-        for path in solution_paths:
-            numerics = []
-            for i in range(len(path)):
-                if i == 0:
-                    transition = self._qp['probabilities'][(self._qp['start_state'], path[i])]
-                else:
-                    transition = self._qp['probabilities'][(path[i - 1], path[i])]
-
-                numerics.append(transition)
-
-            answer += functools.reduce(operator.mul, numerics)
-            numeric_strings.append(r' \times '.join([str(i) for i in numerics]))
-        
-        
-
-        line_1 = r'''Let $X$ represent the number of times {0} goes to {1} over the ${2}$ {3}s'''.format(
-                    self._qi['first_name'], self._qp['cinemas'][self._qp['binomial']['cinema']], self._qp['binomial']['num_trials'], self._qi['day'])
-
-        line_2 = r'''$Pr(X = {0}) = {1}$'''.format(self._qp['binomial']['num_successes'], ' + '.join(path_strings))
-
-        line_3 = r'''$= {0}$'''.format(' + '.join(numeric_strings))
-
+        answer = sum(MarkovChainBinomial.path_value(path, self._qp['transition_matrix']) for path in solution_paths)
         # not sure how to make decimal.Decimal stop at the endless 0s of a decimal
-        line_4 = r'''$= {0}$'''.format(answer)
+        lines += r'''$= {answer}$'''.format(
+            answer=answer
+        )
 
-
-        return latex.latex_newline().join([line_1, line_2, line_3, line_4])
+        return lines.write()
--- maths/questions/tests/test_markov_chain.py
+++ maths/questions/tests/test_markov_chain.py
@@ -1,8 +1,43 @@
 from maths.questions import markov_chain
 from maths.latex.questions import QuestionTree
 from .question_tester import question_tester
+import decimal
+import functools
+import operator
+
+
+transition_matrix = {
+    (0, 0): decimal.Decimal('0.7'), (1, 0): decimal.Decimal('0.4'),
+    (0, 1): decimal.Decimal('0.3'), (1, 1): decimal.Decimal('0.6')
+}
+
+location_names = ['A', 'B']
 
 
 def test_MarkovChain():
     q1 = markov_chain.MarkovChainBinomial()
     question_tester(QuestionTree(part=q1))
+
+
+def test_all_path():
+    all_paths = markov_chain.MarkovChainBinomial.all_paths(start_state=0, n_trials=3)
+    assert set(tuple(path) for path in all_paths) == set(((0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)))
+
+
+def test_cinema_path():
+    path = [0, 1, 1, 0]
+    assert markov_chain.MarkovChainBinomial.cinema_path(path, location_names) == \
+        r'Pr(X_{0} = \text{A}) \times Pr(X_{1} = \text{B}) \times Pr(X_{2} = \text{B}) \times Pr(X_{3} = \text{A})'
+
+
+def test_numeric_path():
+    path = [0, 0, 1, 1, 0]
+    transitions = [decimal.Decimal('0.7'), decimal.Decimal('0.3'), decimal.Decimal('0.6'), decimal.Decimal('0.4')]
+    assert markov_chain.MarkovChainBinomial.numeric_path(path, transition_matrix) == \
+        r' \times '.join(str(i) for i in transitions)
+
+
+def test_path_value():
+    path = [0, 0, 1, 1, 0]
+    transitions = [decimal.Decimal('0.7'), decimal.Decimal('0.3'), decimal.Decimal('0.6'), decimal.Decimal('0.4')]
+    assert markov_chain.MarkovChainBinomial.path_value(path, transition_matrix) == functools.reduce(operator.mul, transitions)
