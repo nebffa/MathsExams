9e43bc653963960803954514e5509b6b66092d32
Ben Lucato <ben.lucato@gmail.com>
2014-04-17 11:26:38 +1000

    Modify our question testing a little bit.

--- sublime_run_as_script.py
+++ sublime_run_as_script.py
@@ -1,14 +1,25 @@
 import sys
 import subprocess
 import os
+import importlib
+from maths.questions import relationships
+from maths.questions.tests import question_tester
 
 
-script_path = sys.argv[1]
-project_path = sys.argv[2]
+if __name__ == '__main__':
+    module_path = sys.argv[1]
+    project_path = sys.argv[2]
 
-script = os.path.relpath(script_path, project_path).replace('/', '.')
+    module_name = os.path.split(module_path)[1]
+    module_name = os.path.splitext(module_name)[0]
 
-python_path = os.path.join(project_path, 'bin', 'python')
+    relative_path = os.path.relpath(module_path, __file__)
+    relative_path = os.path.split(relative_path)[0]
+    relative_path = relative_path.replace('..', '.')
+    relative_path = relative_path.replace('/', '')
 
-os.chdir(project_path)
-subprocess.call([python_path, "-m", script])
+    script_name = os.path.relpath(module_path, project_path).replace('/', '.')[:-3]
+    module = importlib.import_module(script_name)
+
+    question = relationships.parse_structure(module)
+    question_tester.question_tester(question, view_output=True)
