aa7002ad34206719c4e6000301bebb3d1a85fe21
Ben Lucato <ben.lucato@gmail.com>
2014-04-17 11:38:21 +1000

    Cleanup the code in log_misc.py

--- maths/questions/log_misc.py
+++ maths/questions/log_misc.py
@@ -1,7 +1,7 @@
 import sympy
 import random
-from sympy.abc import *
-from .. import all_functions, not_named_yet
+from ..symbols import x, x0, x1
+from .. import all_functions
 from ..utils import sensible_values
 from ..latex import solution_lines
 import functools
@@ -10,140 +10,226 @@ from . import relationships
 
 
 @relationships.root
-class SolveLogEquation:
-    def __init__(self):
-        # 2008 Q9 [7 lines] [4 marks]
-        # 2012 Q7 [8 lines] [3 marks]
+class SolveLogEquation(relationships.QuestionPart):
+    """
+    Question description
+    ====================
 
-        self.num_lines, self.num_marks = 8, 3
+    Solve an equation composed of logarithms.
 
-        self._qp = {}
+    Real-life instances
+    ===================
 
+    2009 9: 2*log(x) - log(x + 3) = log(1/2)        [7 lines] [4 marks]
+    2012 7: 2*log(x + 2) - log(x) = log(2*x + 1)    [8 lines] [3 marks]
+    """
+
+    @staticmethod
+    def log_orders():
+        """Return coefficients that determine the order of the logs.
+
+        e.g. 2 * log(x) where 2 is an order
+        """
+
+        orders = [1, 2]
+        random.shuffle(orders)
+        return orders
+
+    @staticmethod
+    def log_coefficients():
+        """Return coefficients that determine whether the logs are positive or negative.
+        """
+
+        coefficients = [-1, 1]
+        random.shuffle(coefficients)
+        return coefficients
+
+    @staticmethod
+    def log_interiors():
+        """Return linear expressions that will be used as the interiors of the logs.
+        """
 
         while True:
-            good = True
-            left_orders = [1, 2]
-            random.shuffle(left_orders)
-            left_coefficients = [-1, 1]
-            random.shuffle(left_coefficients)
+            left_log_interiors = [all_functions.request_linear(difficulty=3).equation for solution in range(2)]
+            if left_log_interiors[0] != left_log_interiors[1]:
+                return left_log_interiors
 
-            while True:
-                left_log_interiors = [all_functions.request_linear(difficulty=3).equation for i in range(2)]
-                if left_log_interiors[0] != left_log_interiors[1]:
-                    break
-            left_logs = [left_coefficients.pop() * left_orders.pop() * sympy.log(left_log_interiors.pop()) for i in range(2)]
-                
+    @staticmethod
+    def build_logs(orders, coefficients, interiors):
+        """Return a set of logs that have been built from:
+            - an order (positive integer)
+            - a coefficient (-1 or +1)
+            - an interior of the logarithm
+
+        >>> SolveLogEquation.build_logs([2, 3], [-1, 1], [2 * x + 1, -3 * x])
+        [-2*log(2*x + 1), 3*log(-3*x)]
+        """
+
+        num_logs = len(orders)
+
+        logs = []
+        for log_number in range(num_logs):
+            log = orders[log_number] * coefficients[log_number] * sympy.log(interiors[log_number])
+            logs.append(log)
+
+        return logs
+
+    @staticmethod
+    def log_solver(expr, check_validity=False):
+        """Return valid solutions (i.e. solutions that don't evaluate any log that's part of the expression as complex)
+        for an expression that is the addition/subtraction of logs.
+
+        >>> SolveLogEquation.log_solver(sympy.log(x - 1))
+        [2]
+
+        >>> SolveLogEquation.log_solver(sympy.log(3 * x - 2) - 2 * sympy.log(x))
+        [1, 2]
+        """
+
+        single_log = sympy.logcombine(expr, force=True)
+        interior = single_log.match(x0 * sympy.log(x1))[x1]
+
+        numerator, denominator = interior.as_numer_denom()
+
+        solutions = sympy.solve(numerator - denominator)
+
+        if check_validity:
+            return [solution for solution in solutions if SolveLogEquation.is_valid_solution(expr, solution)]
+        else:
+            return solutions
+
+    @staticmethod
+    def is_valid_solution(expr, solution):
+        """State whether an expression of logs evaluates as real at a given solution.
+
+        >>> SolveLogEquation.is_valid_solution(sympy.log(x), 2)
+        True
+
+        >>> SolveLogEquation.is_valid_solution(2 * sympy.log(x) - sympy.log(x + 1), -1)
+        False
+        """
+
+        symbol_used = expr.free_symbols.pop()
+
+        for log in expr.find(sympy.log):
+            evaluated = log.subs({symbol_used: solution})
+            if not sympy.ask(sympy.Q.real(evaluated)):
+                return False
+
+        return True
+
+    @staticmethod
+    def logcombine_include_negative_power(expr, force=False):
+        """Perform a more powerful logcombine than SymPy's logcombine.
+
+        In SymPy:
+        logcombine(-log(x)) = -log(x), rather than log(1/x).
+
+        This behaviour is implemented here.
+
+        >>> SolveLogEquation.logcombine_include_negative_power(-sympy.log(2 * x + 1))
+        log(1/(2*x + 1))
+        """
+
+        expr = sympy.logcombine(expr, force)
+
+        if expr.could_extract_minus_sign():
+            interior = expr.match(x0 * sympy.log(x1))[x1]
+            expr *= -1
+            expr = sympy.log(1 / interior)
+
+        return expr
+
+    def __init__(self):
+        self.num_lines, self.num_marks = 8, 3
+        self._qp = {}
+
+        while True:
+            left_orders = SolveLogEquation.log_orders()
+            left_coefficients = SolveLogEquation.log_coefficients()
+            left_log_interiors = SolveLogEquation.log_interiors()
+
+            left_logs = SolveLogEquation.build_logs(left_orders, left_coefficients, left_log_interiors)
+
+            # we have built the logs for one side of the equation, now do the same for the other side
             for log in left_logs:
                 if isinstance(log, sympy.Mul):
                     coeff = log.args[0]
-
                     if coeff == -2:
                         right_log_coeff = -1
                     elif coeff == 2:
                         right_log_coeff = 1
 
-            right_log = right_log_coeff * sympy.log(all_functions.request_linear(difficulty=3).equation)
-                    
+            right_interior = all_functions.request_linear(difficulty=3).equation
+
+            self._qp['left_side'] = functools.reduce(operator.add, left_logs)
+            self._qp['right_side'] = right_log_coeff * sympy.log(right_interior)
+
+            # randomly switch the sides so we don't always have 2 logs on the left and only 1 on the right
             if random.randint(0, 1):
-                self._qp['left_side'] = functools.reduce(operator.add, left_logs)
-                self._qp['right_side'] = right_log
-            else:
-                self._qp['left_side'] = right_log
-                self._qp['right_side'] = functools.reduce(operator.add, left_logs)
+                self._qp['left_side'], self._qp['right_side'] = self._qp['right_side'], self._qp['left_side']
 
+            self._qp['maybe_invalid_solutions'] = SolveLogEquation.log_solver(self._qp['left_side'] - self._qp['right_side'])
+            self._qp['valid_solutions'] = SolveLogEquation.log_solver(self._qp['left_side'] - self._qp['right_side'], check_validity=True)
 
-            combined_log = sympy.logcombine(self._qp['left_side'] - self._qp['right_side'], force=True)
-            interior = combined_log.args[0]
-
-            numerator, denominator = interior.as_numer_denom()
-
-
-            self._qp['solutions'] = sympy.solve(numerator - denominator)
-
-            if len(self._qp['solutions']) < 2:
+            if len(self._qp['maybe_invalid_solutions']) < 2:  # we need enough solutions so the students have some work to do
+                continue
+            elif len(self._qp['valid_solutions']) == 0:
                 continue
 
+            are_all_solutions_sensible = all([sensible_values.looks_good(solution) for solution in self._qp['maybe_invalid_solutions']])
+            if not are_all_solutions_sensible:
+                continue
 
-            for solution in self._qp['solutions']:
-                if not sensible_values.looks_good(solution):
-                    good = False
-
-            all_logs = (self._qp['left_side'] - self._qp['right_side']).find(sympy.log)
-            all_interiors = [log.args[0] for log in all_logs]
-
-            valid_solution_found = False
-            self._qp['good_solutions'] = []
-            self._qp['bad_solutions'] = []
-            for solution in self._qp['solutions']:
-                if all([interior.subs({x: solution}) > 0 for interior in all_interiors]):
-                    valid_solution_found = True
-                    self._qp['good_solutions'].append(solution)
-                else:
-                    self._qp['bad_solutions'].append(solution)
-
-            if good and valid_solution_found:
-                break
+            break
 
     def question_statement(self):
-        return r'''Solve the equation ${0} = {1}$ for $x$.'''.format(
-            sympy.latex(self._qp['left_side']), 
-            sympy.latex(self._qp['right_side'])
+        return r'''Solve the equation ${left_side} = {right_side}$ for $x$.'''.format(
+            left_side=sympy.latex(self._qp['left_side']),
+            right_side=sympy.latex(self._qp['right_side'])
         )
-
 
     def solution_statement(self):
         lines = solution_lines.Lines()
 
-        left_combined = sympy.logcombine(self._qp['left_side'], force=True)
-        right_combined = sympy.logcombine(self._qp['right_side'], force=True)
-
-        
-
-        if left_combined.could_extract_minus_sign():
-            left_combined *= -1
-            interior = left_combined.args[0]
-            left_combined = sympy.log(1 / interior)
-
-        if right_combined.could_extract_minus_sign():
-            right_combined *= -1
-            interior = right_combined.args[0]
-            right_combined = sympy.log(1 / interior)
-                
-        
-
-
-        lines += r'${0} = {1}$'.format(sympy.latex(left_combined), sympy.latex(right_combined))
-        
-        lines += r'$\therefore {0} = {1}$'.format(
-            sympy.latex(left_combined.args[0]),
-            sympy.latex(right_combined.args[0])
+        left_combined = SolveLogEquation.logcombine_include_negative_power(self._qp['left_side'], force=True)
+        right_combined = SolveLogEquation.logcombine_include_negative_power(self._qp['right_side'], force=True)
+        lines += r'${left_combined} = {right_combined}$'.format(
+            left_combined=sympy.latex(left_combined),
+            right_combined=sympy.latex(right_combined)
         )
 
+        lines += r'$\therefore {left_interior} = {right_interior}$'.format(
+            left_interior=sympy.latex(left_combined.args[0]),
+            right_interior=sympy.latex(right_combined.args[0])
+        )
 
         left_numer, left_denom = left_combined.args[0].as_numer_denom()
         right_numer, right_denom = right_combined.args[0].as_numer_denom()
-
-        lines += r'${0} = {1}$'.format(
-            sympy.latex((left_numer * right_denom).expand()),
-            sympy.latex((right_numer * left_denom).expand())
+        lines += r'${left_intermediate} = {right_intermediate}$'.format(
+            left_intermediate=sympy.latex((left_numer * right_denom).expand()),
+            right_intermediate=sympy.latex((right_numer * left_denom).expand())
         )
 
-        quadratic = (left_numer * right_denom - right_numer * left_denom).expand()
-        if quadratic.coeff(x**2).could_extract_minus_sign():
-            quadratic *= -1
-
-        lines += r'${0} = 0$'.format(
-            sympy.latex(quadratic)
+        collected_like_terms = (left_numer * right_denom - right_numer * left_denom).expand()
+        lines += r'${collected_like_terms} = 0$'.format(
+            collected_like_terms=sympy.latex(collected_like_terms)
         )
 
-        solutions = sympy.solve(quadratic)
-        lines += r'$x = {0}$'.format(
-            ', '.join([sympy.latex(i) for i in solutions])
+        solutions = sympy.solve(collected_like_terms)
+        lines += r'$x = {solutions}$'.format(
+            solutions=', '.join([sympy.latex(solution) for solution in solutions])
         )
 
-        if len(self._qp['bad_solutions']) > 0:
-            lines += r'but $x = {0}$ is an invalid solution since a log can only accept positive values'.format(sympy.latex(self._qp['bad_solutions'][0]))
-            lines += r'$\therefore x = {0}$'.format(sympy.latex(self._qp['good_solutions'][0]))
+        invalid_solutions = set(self._qp['maybe_invalid_solutions']) - set(self._qp['valid_solutions'])
+        if len(invalid_solutions) > 0:
+            invalid_solution = invalid_solutions.pop()
+            lines += r'but $x = {invalid_solution}$ is an invalid solution since a log can only accept positive values'.format(
+                invalid_solution=sympy.latex(invalid_solution)
+            )
 
+            lines += r'$\therefore x = {valid_solution}$'.format(
+                valid_solution=', '.join([sympy.latex(i) for i in self._qp['valid_solutions']])
+            )
 
         return lines.write()
