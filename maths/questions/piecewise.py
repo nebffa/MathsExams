6f80885d90d38f9101673c872542538f403159ee
Ben Lucato <ben.lucato@gmail.com>
2014-04-27 17:49:09 +1000

    Cleanup the code in piecewise.

--- maths/questions/piecewise.py
+++ maths/questions/piecewise.py
@@ -1,30 +1,24 @@
 import sympy
 import random
-from sympy.abc import *
 from ..rich_requests import requests
 from ..plot import plot
 from ..utils import functions
-from ..symbols import *
+from ..symbols import x, coeff0, coeff1, coeff2, coeff3
 from . import relationships
+import copy
 
 
 @relationships.root
-class Piecewise:
+class Piecewise(relationships.QuestionPart):
     def __init__(self):
         self.num_lines, self.num_marks = 0, 0
-        
-        self._question_params = {}
-
-        partition_x = random.randint(-2, 2)
+        self._qp = {}
 
         function_left, function_right = random.sample(['hyperbola', 'cubic', 'absolute_value'], 2)
 
-        if random.randint(0, 1):  # decide which part of the Piecewise gets ownership of the shared x-coordinate
-            domain_left = sympy.Interval(-sympy.oo, partition_x, False, True)
-            domain_right = sympy.Interval(partition_x, sympy.oo, False, False)
-        else:
-            domain_left = sympy.Interval(-sympy.oo, partition_x, False, False)
-            domain_right = sympy.Interval(partition_x, sympy.oo, True, False)
+        partition_x = random.randint(-2, 2)
+        domain_left = sympy.Interval(-sympy.oo, partition_x, False, True)
+        domain_right = sympy.Interval(partition_x, sympy.oo, False, False)
 
         if function_left == 'hyperbola':
             function_left = requests.hyperbola(vertical_asymptote=domain_left)
@@ -38,102 +32,114 @@ class Piecewise:
         elif function_right == 'cubic':
             function_right = requests.cubic(turning_points_location=domain_right, num_turning_points=2)
         elif function_right == 'absolute_value':
-            function_right = requests.absolute_value(extreme_point=domain_right)        
+            function_right = requests.absolute_value(extreme_point=domain_right)
 
-        self.equation = sympy.Piecewise((function_left, x < domain_left.right), (function_right, x >= domain_left.right))
-        self._question_params['plot_domain'] = self._question_params['plot_range'] = sympy.Interval(-6, 6)
+        if random.randint(0, 1):  # decide which part of the piecewise function gets ownership of the shared x-coordinate
+            self._qp['equation'] = sympy.Piecewise((function_left, x < domain_left.right), (function_right, x >= domain_left.right))
+        else:
+            self._qp['equation'] = sympy.Piecewise((function_left, x <= domain_left.right), (function_right, x > domain_left.right))
+
+        self._qp['plot_domain'] = self._qp['plot_range'] = sympy.Interval(-6, 6)
 
     def question_statement(self):
-        path = plot.plot(self.equation, self._question_params['plot_domain'], self._question_params['plot_range'])
+        path = plot.plot(self._qp['equation'], self._qp['plot_domain'], self._qp['plot_range'])
 
-        return r'''The graph of the function $f$ is shown, where ${0}$
-                        {1}'''.format(sympy.latex(self.equation), plot.latex(path))
-        
-    def solution_statement(self):
-        return ''
+        return r'''The graph of the function $f$ is shown, where $f = {equation}$ {plot}'''.format(
+            equation=sympy.latex(self._qp['equation']),
+            plot=plot.latex(path)
+        )
 
     def sanity_check(self):
         # check that the middle point does not have the same y-value and same derivative. otherwise that would be an oopsie!!
-        middle = self.equation.args[0].args[1].rhs
-        func_left, func_right = self.equation.args[0].args[0], self.equation.args[1].args[0]
+        middle = self._qp['equation'].args[0].args[1].rhs
+        func_left, func_right = self._qp['equation'].args[0].args[0], self._qp['equation'].args[1].args[0]
 
-        if (func_left.subs({x: middle}) == func_right.subs({x: middle}) and 
-            func_left.diff().subs({x: middle}) == func_right.diff().subs({x: middle}) ):
+        func_left_value_at_middle = func_left.subs({x: middle})
+        func_right_value_at_middle = func_right.subs({x: middle})
+
+        left_slope_at_middle = func_left.diff().subs({x: middle})
+        right_slope_at_middle = func_right.diff().subs({x: middle})
+
+        same_y_coordinate_at_middle = True if func_left_value_at_middle == func_right_value_at_middle else False
+        same_derivative_at_middle = True if left_slope_at_middle == right_slope_at_middle else False
+
+        if same_y_coordinate_at_middle and same_derivative_at_middle:
             raise ValueError('The piecewise function is differentiable at its split point!')
-        
+
 
 @relationships.is_child_of(Piecewise)
-class DomainDerivative:
+class DomainDerivative(relationships.QuestionPart):
     def __init__(self, part):
         self.num_lines, self.num_marks = 3, 1
+        self._qp = copy.copy(part._qp)
 
-        self.equation = part.equation
-        self.domain = sympy.Interval(-sympy.oo, sympy.oo)
-        for piecewise_part in self.equation.args:
+        self._qp['domain'] = sympy.Interval(-sympy.oo, sympy.oo)
+        for piecewise_part in self._qp['equation'].args:
             expr = piecewise_part[0]
 
-            self.domain &= functions.maximal_domain(expr, self.domain)
+            self._qp['domain'] &= functions.maximal_domain(expr, self._qp['domain'])
 
-            if expr.find(sympy.Abs):
-                match = expr.match(x0 * sympy.Abs(x1 * x - x2) + x3)
-                self.domain -= sympy.FiniteSet(match[x2] / match[x1])
+            if expr.find(sympy.Abs):  # an absolute value is not differentiable at its vertex
+                match = expr.match(coeff0 * sympy.Abs(coeff1 * x - coeff2) + coeff3)
+                self._qp['domain'] -= sympy.FiniteSet(match[coeff2] / match[coeff1])
 
-        middle = self.equation.args[0].args[1].rhs
-        self.domain -= sympy.FiniteSet(middle)
+        # a piecewise function is not differentiable at the meeting points of the subparts (unless the same y-coordinate
+        # and the same derivative is approached from both sides, which it is guaranteed not to)
+        middle = self._qp['equation'].args[0].args[1].rhs
+        self._qp['domain'] -= sympy.FiniteSet(middle)
 
-        
     def question_statement(self):
-        return r"""The stationary points of the function are labelled with their coordinates. Write down the domain of the derivative function $f'$."""
+        return r"""The stationary points of the function are labelled with their coordinates.
+            Write down the domain of the derivative function $f'$."""
 
     def solution_statement(self):
-        return r'''${0}$'''.format(sympy.latex(self.domain))
-
-    def sanity_check(self):
-        pass
+        return r'${answer}$'.format(
+            answer=sympy.latex(self._qp['domain'])
+        )
 
 
 @relationships.is_child_of(Piecewise)
-class AbsoluteValue:
+class AbsoluteValue(relationships.QuestionPart):
     def __init__(self, part):
         self.num_lines, self.num_marks = 0, 2
-        self._question_params = {}
-        self._question_params['plot_domain'] = part._question_params['plot_domain']
-        self._question_params['plot_range'] = part._question_params['plot_range']
-
+        self._qp = copy.copy(part._qp)
 
         # find the half of the piecewise that is not already an absolute value
-        func_left, func_right = part.equation.args
-        if func_left[0].has(sympy.Abs):
+        func_left, func_right = self._qp['equation'].args
+        if func_left.has(sympy.Abs):
             self.function = func_right
-            self._question_params['equation'] = sympy.Abs(func_right[0])
-            self._question_params['domain'] = functions.relation_to_interval(func_right[1]) & self._question_params['plot_domain']
+            self._qp['equation'] = sympy.Abs(func_right[0])
+            self._qp['domain'] = functions.relation_to_interval(func_right[1]) & self._qp['plot_domain']
         else:
-            self._question_params['equation'] = sympy.Abs(func_left[0])
-            self._question_params['domain'] = functions.relation_to_interval(func_left[1]) & self._question_params['plot_domain']
+            self._qp['equation'] = sympy.Abs(func_left[0])
+            self._qp['domain'] = functions.relation_to_interval(func_left[1]) & self._qp['plot_domain']
 
-        self._question_params['is_cubic'] = self._question_params['equation'].has(x**3)
-
+        self._qp['is_cubic'] = self._qp['equation'].has(x ** 3)
 
     def question_statement(self):
-        path = plot.blank_plot(self._question_params['plot_domain'], self._question_params['plot_range'])
+        path = plot.blank_plot(self._qp['plot_domain'], self._qp['plot_range'])
 
-        if self._question_params['is_cubic']:
-            cubic_statement = r'''Label stationary points with their coordinates (do not attempt to find x-axis intercepts).'''
-        else: 
+        if self._qp['is_cubic']:
+            cubic_statement = r'Label stationary points with their coordinates (do not attempt to find x-axis intercepts).'
+        else:
             cubic_statement = r''
 
-        return r'''By referring to the graph of $f$, sketch the graph of the function with rule 
-            $y = {0}$, for ${1}$. {2} {3}'''.format(sympy.latex(self._question_params['equation']), 
-                                                    sympy.latex(self._question_params['domain']), 
-                                                    cubic_statement, 
-                                                    plot.latex(path))
-
+        return r'''By referring to the graph of $f$, sketch the graph of the function with rule
+            $y = {equation}$, for ${domain}$. {cubic_statement} {plot}'''.format(
+            equation=sympy.latex(self._qp['equation']),
+            domain=sympy.latex(self._qp['domain']),
+            cubic_statement=cubic_statement,
+            plot=plot.latex(path)
+        )
 
     def solution_statement(self):
-        path = plot.plot(self._question_params['equation'], self._question_params['plot_domain'], self._question_params['plot_range'],
-                            expr_domain=self._question_params['domain'])
+        path = plot.plot(
+            self._qp['equation'],
+            self._qp['plot_domain'],
+            self._qp['plot_range'],
+            expr_domain=self._qp['domain']
+        )
 
-        return r'''{0}'''.format(plot.latex(path))
-
-    def sanity_check(self):
-        pass
+        return r'''{plot}'''.format(
+            plot=plot.latex(path)
+        )
--- maths/questions/tests/test_piecewise.py
+++ maths/questions/tests/test_piecewise.py
@@ -1,9 +1,9 @@
-from maths.questions import piecewise
+from maths.questions import piecewise, relationships
 from maths.latex.questions import QuestionTree
 from .question_tester import question_tester
 import pytest
 
 
 def test_Piecewise():
-    q1 = piecewise.Piecewise()
-    question_tester(QuestionTree(q1))
+    question = relationships.parse_structure(piecewise)
+    question_tester(question)
