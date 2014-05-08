782de2886147abcdd4b7001546456fdcbe809f8c
Ben Lucato <ben.lucato@gmail.com>
2014-05-08 18:37:10 +1000

    Cleanup the code in simple_sketch.

--- maths/questions/simple_sketch.py
+++ maths/questions/simple_sketch.py
@@ -1,16 +1,16 @@
 import sympy
-from sympy.abc import *
+from ..symbols import x, y
 from ..rich_requests import requests
 from ..plot import plot
 from ..utils import transformations, noevals
 import random
 import copy
-from ..latex import latex, solutions
+from ..latex import solutions
 from . import relationships
 
 
 @relationships.root
-class SimpleSketch:
+class SimpleSketch(relationships.QuestionPart):
     def __init__(self):
 
         self.num_lines, self.num_marks = 0, 3
@@ -22,31 +22,37 @@ class SimpleSketch:
 
         self._qp['domain'] = sympy.Interval(random.randint(-5, -3), random.randint(3, 5))
 
-
         if function_type == 'absolute_value':
             self._qp['equation'] = requests.absolute_value(extreme_point=self._qp['domain'])
         elif function_type == 'hyperbola':
-            self._qp['equation'] = requests.hyperbola(vertical_asymptote=self._qp['domain'],
-                                            horizontal_asymptote=self._qp['plot_range'])
+            self._qp['equation'] = requests.hyperbola(
+                vertical_asymptote=self._qp['domain'],
+                horizontal_asymptote=self._qp['plot_range']
+            )
 
-    
     def question_statement(self):
         path = plot.blank_plot(self._qp['plot_domain'], self._qp['plot_range'])
 
-        return r'''Sketch the graph of $f: {0} \rightarrow R, f(x) = {1}$. Label the axes intercepts and endpoints with their coordinates. {2}'''.format(
-                    sympy.latex(self._qp['domain']), sympy.latex(self._qp['equation']), plot.latex(path))
-
+        return r'''Sketch the graph of $f: {domain} \rightarrow R, f(x) = {equation}$. Label the axes intercepts and
+            endpoints with their coordinates. {plot}'''.format(
+            domain=sympy.latex(self._qp['domain']),
+            equation=sympy.latex(self._qp['equation']),
+            plot=plot.latex(path)
+        )
 
     def solution_statement(self):
-        path = plot.plot(self._qp['equation'], self._qp['plot_domain'], self._qp['plot_range'],
-                            expr_domain=self._qp['domain'])
-
+        path = plot.plot(
+            self._qp['equation'],
+            self._qp['plot_domain'],
+            self._qp['plot_range'],
+            expr_domain=self._qp['domain']
+        )
 
         return r'''{0}'''.format(plot.latex(path))
 
 
 @relationships.is_child_of(SimpleSketch)
-class PointTransformation:
+class PointTransformation(relationships.QuestionPart):
     def __init__(self, part):
         self.num_lines, self.num_marks = 4, 1
         self._qp = copy.deepcopy(part._qp)
@@ -59,25 +65,28 @@ class PointTransformation:
         self._qp['transformations'] = transformations.random_transformation(num_transformations=2)
 
     def question_statement(self):
-        return r'''Find the coordinates of the image of the point ${0}$ {1}.'''.format(
-                    sympy.latex(self._qp['point']), 
-                    transformations.print_transformations(self._qp['transformations']) 
-                    )
+        return r'''Find the coordinates of the image of the point ${point}$ {description_of_transformations}.'''.format(
+            point=sympy.latex(self._qp['point']),
+            description_of_transformations=transformations.description_of_transformations(self._qp['transformations'])
+        )
 
     def solution_statement(self):
+        lines = solutions.Lines()
+
         mapping = transformations.show_mapping(self._qp['transformations'])
+        lines += r'${0}$'.format(mapping)
+
         mapped_point = transformations.apply_transformations(self._qp['transformations'], self._qp['point'])
+        lines += r'$\therefore {point} \rightarrow {mapped_point}$'.format(
+            point=sympy.latex(self._qp['point']),
+            mapped_point=sympy.latex(mapped_point),
+        )
 
-        therefore = r'$\therefore {0} \rightarrow {1}$'.format(
-                        sympy.latex(self._qp['point']), 
-                        sympy.latex(mapped_point), 
-                        )
-
-        return r'''${0}$'''.format(mapping) + latex.latex_newline() + therefore
+        return lines.write()
 
 
 @relationships.is_child_of(SimpleSketch)
-class EquationTransformation:
+class EquationTransformation(relationships.QuestionPart):
     def __init__(self, part):
         self.num_lines, self.num_marks = 8, 2
         self._qp = copy.deepcopy(part._qp)
@@ -86,34 +95,42 @@ class EquationTransformation:
 
     def question_statement(self):
         return r'''Find the equation of the image of the graph of $f$ {0}.'''.format(
-                        transformations.print_transformations(self._qp['transformations']) )
+            transformations.description_of_transformations(self._qp['transformations'])
+        )
 
     def solution_statement(self):
-        mapping_chain = transformations.show_mapping(self._qp['transformations'])
+        lines = solutions.Lines()
+
         mapping = transformations.overall_transformation(self._qp['transformations'])
         reversed_mapping = transformations.reverse_mapping(mapping)
         answer = transformations.apply_transformations(self._qp['transformations'], self._qp['equation'])
 
-        
-        lines = solutions.Lines()
-
+        mapping_chain = transformations.show_mapping(self._qp['transformations'])
         lines += r'${0}$'.format(mapping_chain)
 
         x_, y_ = sympy.symbols("x' y'")
         new_coords = (x_, y_)
-        lines += r"Let our new ${0}$ coordinates be ${1}$.".format(sympy.latex((x, y)), sympy.latex((x_, y_)))
+        lines += r"Let our new ${old_x_y_coordinates}$ coordinates be ${new_x_y_coordinates}$.".format(
+            old_x_y_coordinates=sympy.latex((x, y)),
+            new_x_y_coordinates=sympy.latex((x_, y_))
+        )
 
-
-        reversed_mapping = tuple(i.subs({x: x_, y: y_}) for i in reversed_mapping)        
-        lines += r'Hence, ${0} \rightarrow {1}$ and ${2} \rightarrow {3}$'.format(sympy.latex(mapping), sympy.latex(new_coords),
-                            sympy.latex((x, y)), sympy.latex(reversed_mapping))
-
+        reversed_mapping = tuple(i.subs({x: x_, y: y_}) for i in reversed_mapping)
+        lines += r'Hence, ${mapping} \rightarrow {new_mapping}$ and ${old_x_y_coordinates} \rightarrow {new_x_y_coordinates}$'.format(
+            mapping=sympy.latex(mapping),
+            new_mapping=sympy.latex(new_coords),
+            old_x_y_coordinates=sympy.latex((x, y)),
+            new_x_y_coordinates=sympy.latex(reversed_mapping)
+        )
 
         lines += 'Now we apply the mapping to the equation:'
 
         noevaled_equation = noevals.noevalify(self._qp['equation'])
         noevalmapped_equation = noevaled_equation.subs({x: reversed_mapping[0]})
-        lines += r'${0} = {1}$'.format(sympy.latex(reversed_mapping[1]), sympy.latex(noevalmapped_equation))
+        lines += r'${0} = {1}$'.format(
+            sympy.latex(reversed_mapping[1]),
+            sympy.latex(noevalmapped_equation)
+        )
 
         lines += r'Hence our mapped equation is $y = {0}$.'.format(sympy.latex(answer.apart()))
 
