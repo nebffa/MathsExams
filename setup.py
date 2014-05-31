03a79d316f71e3625008a87f4832f02f8f723ecd
Ben Lucato <ben.lucato@enett.com>
2014-05-31 12:54:40 +1000

    Introduce dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import os
 from setuptools import setup
+from pip.req import parse_requirements
+
+
+install_requirements = parse_requirements('requirements.txt')
+requirements = [str(install_requirement.requirement) for install_requirement in install_requirements]
 
 
 setup(
@@ -8,5 +13,6 @@ setup(
     author_email="ben.lucato@gmail.com",
     description="Create maths questions for students to practice with",
     url="https://github.com/nebffa/MathsExams",
-    packages=["maths"]
+    packages=["maths"],
+    install_requires=requirements
 )
