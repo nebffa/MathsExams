2c542bd888158cd04148d7a1e85815db905431b5
Ben Lucato <ben.lucato@gmail.com>
2014-05-31 17:12:23 +1000

    moooore work on packaging

--- setup.py
+++ setup.py
@@ -8,6 +8,23 @@ setup(
     author_email="ben.lucato@gmail.com",
     description="Create maths questions for students to practice with",
     url="https://github.com/nebffa/MathsExams",
+    packages=[
+        'maths',
+        'maths.api',
+        'maths.debug',
+        'maths.exams',
+        'maths.latex',
+        'maths.phrasing',
+        'maths.plot',
+        'maths.probability',
+        'maths.questions',
+        'maths.relations',
+        'maths.rich_requests',
+        'maths.scripts',
+        'maths.templates',
+        'maths.tests',
+        'maths.utils'
+    ],
     install_requires=[
         "gmpy==1.17",
         "numpy==1.8.0",
